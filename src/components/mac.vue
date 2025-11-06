842efae7ad65b2188166e6b1b1fd546215a7e025
Miguel Duran :) <miguel.duran.14.17@outlook.com>
2025-11-06 16:11:16 -0500

    previo a pintar todo

--- src/components/mac.vue
+++ src/components/mac.vue
@@ -4,41 +4,70 @@ import axios from "axios";
 import Swal from "sweetalert2";
 
 let pok = ref("".toLowerCase());
-let resultado = ref(null);
+let resultado = ref("");
+let imagensrc = ref("");
+
+const coloresTipo = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
+};
 
 function buscar() {
   if (pok.value == "") {
     Swal.fire({
       icon: "error",
       title: "Oops...",
-      text: "Por favor ingresa el nombre o codigo de un Pokémon!",
+      text: "Por favor ingresa el nombre o codigo de un Pokémon valido!",
     });
     return;
-  } 
-    let config = {
-      method: "get",
-      maxBodyLength: Infinity,
-      url: `https://pokeapi.co/api/v2/pokemon/${pok.value}`,
-      headers: {},
-    };
-
-    axios
-      .request(config)
-      .then((response) => {
-        resultado.value = response.data;
-        console.log(resultado.value);
-      })
-      .catch((error) => {
-        console.log("Pokémon no encontrado 😢", error);
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Pokémon no encontrado 😢",
-        })
-        pok.value = "";
-      });
   }
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `https://pokeapi.co/api/v2/pokemon/${pok.value}`,
+    headers: {},
+  };
 
+  axios
+    .request(config)
+    .then((response) => {
+      resultado.value = response.data;
+
+      imagensrc.value =
+        resultado.value.sprites.other.dream_world.front_default ||
+        resultado.value.sprites.other["official-artwork"].front_default ||
+        resultado.value.sprites.front_default;
+
+      console.log(resultado.value);
+    })
+    .catch((error) => {
+      console.log("Pokémon no encontrado 😢", error);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Pokémon no encontrado 😢",
+      });
+      pok.value = "";
+      resultado.value = "";
+      imagensrc.value = "";
+    });
+}
 </script>
 
 
@@ -59,6 +88,13 @@ function buscar() {
       />
       <button @click="buscar" class="bt">buscar</button>
     </div>
+    <div id="card" v-if="resultado">
+      <h2>{{ resultado.name.toUpperCase() }}</h2>
+      <img :src="imagensrc" alt="imagen" />
+    </div>
+
+    <div v-else>
+</div>
   </div>
 </template> 
 
@@ -122,5 +158,10 @@ function buscar() {
 .bt:focus {
   outline: none;
   background-color: #cc0000;
+}
+#card img {
+  width: 200px;
+  height: 200px;
+  object-fit: contain;
 }
 </style>
